5ea0cb3489897e55742e369bb81cfb3b8851fe2d
arturLink <93324246+arturLink@users.noreply.github.com>
2021-11-30 09:52:01 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,36 @@
-﻿using System;
+using System;
 
-namespace ConsoleApp1
+namespace ConsoleAppudde
 {
     class Program
     {
         static void Main(string[] args)
         {
             // login: admin
-            // password: pass1234
+            // password: pass
 
             //declare variables
             //assign value
             string login = "admin";
             string passworrd = "pass";
-
+            int tries = 0;
             string userName, userPassword;
-            Console.WriteLine("Enter your login: ");
-            userName = Console.ReadLine();
-            Console.WriteLine("Enter your password");
-            userPassword = Console.ReadLine();
+            while (tries != 3)
+            {
+                Console.WriteLine("Enter your login: ");
+                userName = Console.ReadLine();
+                Console.WriteLine("Enter your password");
+                userPassword = Console.ReadLine();
 
-            if (login == userName && passworrd == userPassword)
-            {
-                Console.WriteLine("Welcome bud");
-            }
-            else
-            {
-                Console.WriteLine("Goodbye bud");
+                if (login != userName || passworrd != userPassword)
+                {
+                    Console.WriteLine($"oopsie. Something went wrong{tries}");
+                    tries++;
+                }
+                else
+                {
+                    Console.WriteLine("welcome");
+                }
             }
         }
     }
